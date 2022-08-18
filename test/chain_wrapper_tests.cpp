9826028f0a2c96cde5038ebb229c3eb2b9357e27
IvanKostruba <illahad@gmail.com>
2022-08-19 00:24:30 +0200

    Another wrapper test.

--- test/chain_wrapper_tests.cpp
+++ test/chain_wrapper_tests.cpp
@@ -4,6 +4,7 @@
 #include "chain_wrapper.h"
 
 #include <optional>
+#include <unordered_map>
 #include <vector>
 
 #include <gtest/gtest.h>
@@ -48,5 +49,50 @@ TEST(ChainWrapperTests, IndependentState) {
     ASSERT_TRUE(operationSequences[0].is_finished()
         && operationSequences[1].is_finished()
         && !operationSequences[2].is_finished()
+    );
+}
+
+enum class Element {
+    SECOND,
+    THIRD,
+    FOURTH
+};
+
+steps_chain::ChainWrapper progressionFactory(Element p) {
+    switch (p) {
+    case Element::SECOND:
+        return steps_chain::StepsChain{ doubleValue };
+    case Element::THIRD:
+        return steps_chain::StepsChain{ doubleValue, doubleValue };
+    case Element::FOURTH:
+        return steps_chain::StepsChain{
+            doubleValue,
+            doubleValue,
+            [](const IntParameter& data){ return IntParameter{ data._value * 2 }; }
+        };
+    }
+}
+
+// step chains of different lengths or of different callables are different types.
+TEST(ChainWrapperTests, WrappingDifferentChainTypes) {
+    std::unordered_map<int, steps_chain::ChainWrapper> elem;
+    elem[2] = progressionFactory(Element::SECOND);
+    elem[3] = progressionFactory(Element::THIRD);
+    elem[4] = progressionFactory(Element::FOURTH);
+    elem[2].run("1");
+    const auto [step_idx_after_0, data_after_0] = elem[2].get_current_state();
+    ASSERT_EQ(step_idx_after_0, 1);
+    ASSERT_EQ(data_after_0, "2");
+    elem[3].run("3");
+    const auto [step_idx_after_1, data_after_1] = elem[3].get_current_state();
+    ASSERT_EQ(step_idx_after_1, 2);
+    ASSERT_EQ(data_after_1, "12");
+    elem[4].run("10");
+    const auto [step_idx_after_2, data_after_2] = elem[4].get_current_state();
+    ASSERT_EQ(step_idx_after_2, 3);
+    ASSERT_EQ(data_after_2, "80");
+    ASSERT_TRUE(elem[2].is_finished()
+        && elem[3].is_finished()
+        && elem[4].is_finished()
     );
 }
